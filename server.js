f52b609af8b00e5ad10232713906d2aebd32a8bb
cristian <cristian.s.aguero@icloud.com>
2023-12-08 10:54:07 +0200

    users CRUD

--- server.js
+++ server.js
@@ -12,14 +12,17 @@ dotenv.config();
 
 
 const allowedOrigins = [process.env.FRONTEND_URL]
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS - from server.js'));
+//         }
+//     }
+// }
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS - from index.js'));
-        }
-    }
+    origin: allowedOrigins
 }
 app.use(cors(corsOptions));
 
