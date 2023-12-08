4dcddb08964e76f0f352f2aeec9f68ee9b8f58ca
cristian <cristian.s.aguero@icloud.com>
2023-12-08 08:21:46 +0200

    cors, folders and users routing

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
+console.log('starting server building...')
+
 import express from 'express';
+import dotenv from 'dotenv';
+import userRoutes from './routes/userRotues.js';
+import cors from 'cors';
 
 const app = express();
+app.use(express.json());
+
+dotenv.config();
 
 
+const allowedOrigins = [process.env.FRONTEND_URL]
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS - from index.js'));
+        }
+    }
+}
+app.use(cors(corsOptions));
+
+
+app.use('/api/users', userRoutes);
 
 
 const PORT = process.env.PORT || 3000;
