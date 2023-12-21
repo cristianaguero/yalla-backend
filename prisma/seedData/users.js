390928cf9338aee2a90c0bdfaf956d6c2c24074c
liorabocc <lioraboccara@gmail.com>
2023-12-21 13:06:35 +0100

    new images for users

--- prisma/seedData/users.js
+++ prisma/seedData/users.js
@@ -3,30 +3,35 @@ export const users = [
         name: "Juan García",
         email: "juan@example.com",
         password: "password123",
+        imageUrl: "https://randomuser.me/api/portraits/men/50.jpg",
         role: "admin"
     },
     {
         name: "María González",
         email: "maria@example.com",
         password: "password456",
+        imageUrl: "https://randomuser.me/api/portraits/women/19.jpg",
         role: "user"
     },
     {
         name: "Pedro Gómez",
         email: "pedro@example.com",
         password: "password789",
+        imageUrl: "https://randomuser.me/api/portraits/men/40.jpg",
         role: "user"
     },
     {
         name: "Laura Rodríguez",
         email: "laura@example.com",
         password: "passwordabc",
+        imageUrl: "https://randomuser.me/api/portraits/women/67.jpg",
         role: "user"
     },
     {
         name: "Carlos Fernández",
         email: "carlos@example.com",
         password: "passwordxyz",
+        imageUrl: "https://randomuser.me/api/portraits/men/9.jpg",
         role: "user"
     }
 ];
